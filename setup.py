54f24cf1b12425fa27634a114eeb9d4b01bd612d
Matt Luongo <mhluongo@gmail.com>
2011-12-11 16:36:50 -0500

    Updated setup.py to v0.1.5.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='neo4django',
-    version='0.1.4.2',
+    version='0.1.5',
     author='Matt Luongo',
     author_email='mhluongo@gmail.com',
     description='A Django/Neo4j ORM layer based on the neo4j.py.',
