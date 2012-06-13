db8d47b553a2381f6ebfbb6a2ede496caaa05353
Chris <wcbeard10@gmail.com>
2012-06-13 15:36:08 -0400

    Cleaning debug statements

--- neo4django/tests/__init__.py
+++ neo4django/tests/__init__.py
@@ -1,27 +1,17 @@
 import requests
 
 def setup():
-    print 'Debugging 1'
     global neo4django, neo4jrestclient, gdb, Person, settings, neo_constants
-    print 'Debugging 1'
     global models
 
-    print 'Debugging 1'
     from django.conf import settings
-    print 'Debugging 2'
     import neo4django, neo4jrestclient.client as neo4jrestclient
-    print 'Debugging 3'
     from neo4django.db import models
-    print 'Debugging 4'
     import neo4jrestclient.constants as neo_constants
-    print 'Debugging 5'
     gdb_set = settings.NEO4J_DATABASES['default']
-    print 'Debugging 6'
-    # import pdb; pdb.set_trace()
     gdb = neo4jrestclient.GraphDatabase('http://%s:%s%s' % 
                         (gdb_set['HOST'], str(gdb_set['PORT']), gdb_set['ENDPOINT']))
 
-    print 'Debugging 7'
     class Person(models.NodeModel):
         name = models.Property()
         age = models.IntegerProperty(indexed=True)
