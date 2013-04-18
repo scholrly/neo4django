718dd785cef23bffe70e5e092ff60ee499d0a351
Matt Luongo <mhluongo@gmail.com>
2013-04-18 14:36:41 -0400

    Inc'd the version to 0.1.8.

--- neo4django/constants.py
+++ neo4django/constants.py
@@ -1,4 +1,4 @@
-VERSION = '0.1.7'
+VERSION = '0.1.8'
 
 INTERNAL_ATTR='_neo4django'
 
--- setup.py
+++ setup.py
@@ -1,13 +1,14 @@
 from setuptools import setup
+from neo4django.constants import VERSION
 
 setup(
     name='neo4django',
-    version='0.1.7',
+    version=VERSION,
     author='Matt Luongo',
     author_email='mhluongo@gmail.com',
-    description='A Django/Neo4j ORM layer based on the neo4j.py.',
+    description='A Django/Neo4j ORM layer.',
     license = 'GPL',
-    url = "http://packages.python.org/neo4django",
+    url = "https://neo4django.readthedocs.org/en/latest/",
     packages=['neo4django','neo4django.db','neo4django.db.models','neo4django.tests', 'neo4django.gremlin'],
     package_dir={'neo4django':'neo4django'},
     package_data={'neo4django':['gremlin/*.groovy']},
@@ -16,8 +17,8 @@ setup(
     install_requires=[
         'decorator>=3.3.1',
         'python-dateutil>=2.0',
-        'neo4jrestclient>=1.6.2-dev',
-        'Django>=1.3,<=1.4.4',
+        'neo4jrestclient>=1.7',
+        'Django>=1.3,<1.5',
         'lucene-querybuilder>=0.1.6'
     ],
     tests_require=[
@@ -26,13 +27,14 @@ setup(
         'nose-regression>=1.0',
     ],
     classifiers=[
-        'Development Status :: 2 - Pre-Alpha',
+        'Development Status :: 3 - Alpha',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
         'License :: OSI Approved :: GNU General Public License (GPL)',
         'Operating System :: POSIX',
     ],
     dependency_links = [
         'https://github.com/mhluongo/jexp/tarball/master#egg=jexp-0.1.2',
-        'https://github.com/versae/neo4j-rest-client/tarball/fa902cb6c8d85de52da9588a4bd51eeb1a66dbd2#egg=neo4jrestclient-1.6.2-dev',
         'https://github.com/kennethreitz/requests/tarball/add6feab02d21967ca35f1572e6202b1dd11b788#egg=requests-0.4.1-py2.6-dev',
   ]
 )
